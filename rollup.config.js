eee534688ed441cbee9e8a6076ca9834b74aa117
k8w <me@k8w.io>
2021-04-24 16:30:29 +0800

    1.0.0

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default [
+    {
+        input: './src/index.ts',
+        output: [{
+            format: 'cjs',
+            file: './dist/index.cjs',
+            banner: require('./scripts/copyright')
+        }],
+        plugins: [
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        target: 'es5',
+                        declaration: false,
+                        declarationMap: false,
+                        module: "esnext"
+                    }
+                }
+            })
+        ]
+    },
+    {
+        input: './src/index.ts',
+        output: [{
+            format: 'es',
+            file: './dist/index.mjs',
+            banner: require('./scripts/copyright')
+        }],
+        plugins: [
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        target: 'es2015',
+                        declaration: false,
+                        declarationMap: false,
+                        module: "esnext"
+                    }
+                }
+            })
+        ]
+    }
+]
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import 'core-js/es/array/find';
+import 'core-js/es/array/find-index';
+import 'core-js/es/promise/finnaly';
+import 'k8w-extend-native';
+
+export { BaseClient } from './BaseClient';
+export { Counter } from './Counter';
+export { Flow } from './Flow';
+export { MsgHandlerManager } from './MsgHandlerManager';
+export { ServiceMapUtil } from './ServiceMapUtil';
+export { TransportDataUtil } from './TransportDataUtil';
+export { TransportOptions } from './TransportOptions';
